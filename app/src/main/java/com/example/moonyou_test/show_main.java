d5e8291699c7b8c6869d6badb706746377d0a273
Cat Hanbit <sbwar@naver.com>
2021-03-04 13:02:04 +0900

    달력 화면

---
+++ app/src/main/java/com/example/moonyou_test/show_main.java
@@ -0,0 +1,14 @@
+package com.example.moonyou_test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class show_main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_main);
+    }
+}
